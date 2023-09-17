9f018b0f6051a8811d99c6126fc5545850a654b3
LucianoNicolasArrieta <lucianoarrieta077@gmail.com>
2023-09-17 12:22:08 -0300

    a

--- src/main/java/com/grupo15/API/Calculo/Grado/Impacto/GradoImpactoController.java
+++ src/main/java/com/grupo15/API/Calculo/Grado/Impacto/GradoImpactoController.java
@@ -1,8 +1,11 @@
 package com.grupo15.API.Calculo.Grado.Impacto;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
