8082ed5bd3ff87aaf11a334b276d606bc0307dcd
Tomas Zubizarreta <tzubizareta@frba.utn.edu.com.ar>
2023-09-17 12:41:38 -0300

    Merge

--- src/main/java/com/grupo15/API/Calculo/Grado/Impacto/GradoImpactoController.java
+++ src/main/java/com/grupo15/API/Calculo/Grado/Impacto/GradoImpactoController.java
@@ -9,21 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
-
 @RestController
 @RequestMapping("/api")
 public class GradoImpactoController {
-    private Resultado resultado;
+    private int resultado;
 
-    @GetMapping("/calcularImpacto {entidad} {tiempoResolucionIncidente} {cantIncidentesNoResueltos} {cnf} {totalPersonasImpactadas}")
-    public ResponseEntity<Void> calculateImpactGet(@RequestBody String entidad, Integer tiempoResolucionIncidente, Integer cantIncidentesNoResueltos, Integer cnf, Integer totalPersonasImpactadas) {
-        resultado.resultado = (tiempoResolucionIncidente + cantIncidentesNoResueltos * cnf) * totalPersonasImpactadas;
-        resultado.entidadResultado = entidad;
-        return (ResponseEntity<Void>) ResponseEntity.ok();
-    }
-
-    @PostMapping("/calcularImpacto")
-    public ResponseEntity<Resultado> calculateImpactPost() {
     @GetMapping("/calcularImpacto")
     public List<EntidadValor> calculateImpactGet() {
         return RepositorioResultados.getInstance().obtenerTodosLosResultadosOrdenados();
@@ -33,9 +23,9 @@ public class GradoImpactoController {
     public ResponseEntity<Integer> calculateImpactPost(@RequestBody ListadoValores listado) {
         for (ValoresFormula valoresFormula:listado.getValoresPorEntidad()) {
             double gradoImpacto = CalculadoraGradoImpacto.getInstance().calcularGradoImpacto(valoresFormula.tiempoResolucionIncidente,
-                valoresFormula.cantIncidentesNoResueltos,
-                valoresFormula.cnf,
-                valoresFormula.totalPersonasImpactadas);
+                    valoresFormula.cantIncidentesNoResueltos,
+                    valoresFormula.cnf,
+                    valoresFormula.totalPersonasImpactadas);
             RepositorioResultados.getInstance().guardarResultado(new EntidadValor(valoresFormula.entidad_id, gradoImpacto));
         }
         return ResponseEntity.ok(resultado);
